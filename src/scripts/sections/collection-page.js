1dd19832e9fa24951edfb3d65ce1f75df294bca4
sanj718 <Ss1886626>
2020-05-12 21:09:24 +0500

    collections add slider

--- src/scripts/sections/collection-page.js
+++ src/scripts/sections/collection-page.js
@@ -4,6 +4,31 @@ import _ from 'jquery-match-height';
 theme.Collection = (function() {
   $('.template-collection .collection-grid-item__match-height').matchHeight();
   // $('.swiper-slide .collection-grid-item').matchHeight();
+  $('.slider-parent').slick({
+    arrows: true,
+    vertical: false,
+    prevArrow: '<div class="slick-arrow slick-prev wolf-arrow"></div>',
+    nextArrow: '<div class="slick-arrow slick-next wolf-arrow wolf-arrow--right"></div>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1
+        },
+      }
+    ]
+  });
+  
 })();
 
 
